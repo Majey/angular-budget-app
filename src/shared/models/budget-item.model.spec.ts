194e6f50580c75f195a18f740c90491d22e69556
Majey <kevinerick78@yahoo.com>
2021-10-31 19:47:29 +0300

    first commit

---
+++ src/shared/models/budget-item.model.spec.ts
@@ -0,0 +1,7 @@
+import { BudgetItem } from './budget-item.model';
+
+describe('BudgetItem', () => {
+  it('should create an instance', () => {
+    expect(new BudgetItem()).toBeTruthy();
+  });
+});
